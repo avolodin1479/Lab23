b5e40aac83550d42fc8629287a6fcb80f09a1412
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-10-29 22:52:52 +0600

    Добавьте файлы проекта.

---
+++ Lab23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial();
+            Console.ReadKey();
+        }
+        static void Factorial()
+        {
+            int n = 65;
+            long fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            Console.Write("{0}!={1}", n, fact);
+        }
+        static async Task FactorialAsync()
+        {
+            await Task.Run(() => Factorial());
+        }
+    }
+}
